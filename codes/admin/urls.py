1fd11fb01b5ac46160ea362d8ec2cdbe2a38fd48
adem oztas <adem@oztas.net>
2020-08-06 19:12:47 -0400

    change dashboard url

--- codes/admin/urls.py
+++ codes/admin/urls.py
@@ -6,7 +6,7 @@ from django.urls import path
 from backend import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('dashboard/', views.dashboard),
+    # path('admin/', admin.site.urls),
+    path('/', views.dashboard),
 
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
