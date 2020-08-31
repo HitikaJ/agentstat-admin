c5d732944036860fa8ea01ebb8a435f7af8af722
Hassan Ejaz <hassanejaz22@gmail.com>
2020-08-31 12:21:19 +0500

    profile dispute inprogress

--- codes/www/media/js/page/messages.js
+++ codes/www/media/js/page/messages.js
@@ -1,3 +1,5 @@
+var reclaimId = '';
+
 function diffOfHours(createdAt) {
     var pastDate = new Date(createdAt);
 
@@ -20,7 +22,40 @@ function decisionDeadline(createdAt) {
     return (hours > 24) ? 0 : 48-hours;
 }
 
-$(document).ready(function(){
+function disputePendingShow(open=true) {
+    if (open) {
+        $('.dispute-pending-section').show();
+        $('.dispute-decided-section').hide();
+    } else {
+        $('.dispute-pending-section').hide();
+        $('.dispute-decided-section').show();
+    }
+}
+
+function backOpenProfile() {
+    $("#profileDisputeOpendataTable").parent().show(); 
+    $("#profileDisputeOpendataTable_wrapper").show();
+    $('.profileDisputeInfo').hide();
+}
+
+function backCloseProfile() {
+    $("#profileDisputeCloseddataTable").parent().show(); 
+    $("#profileDisputeCloseddataTable_wrapper").show();
+    $('.closedProfileDisputeInfo').hide();
+}
+
+function decisionInfavour(status) {
+    if (status=='pending') {
+        var text = 'Pending';
+    } else if (status=='accept') {
+        var text = 'Disputee';
+    } else {
+        var text = 'Current Owner';
+    }
+    return text;
+}
+
+function initDisputePending() {
     $('#profileDisputeOpendataTable').DataTable( {
         "processing": true,
         "serverSide": true,
@@ -79,48 +114,183 @@ $(document).ready(function(){
             $(row).attr('data-id', data.id);
         }
     });
+}
 
+// jQuery('#profileDisputeCloseddataTable').DataTable( {
+//     data: profileDisputeCloseddataSet,
+//     "bLengthChange": false,
+//     "bFilter": false,
+//     "bSort":false,
+//     bAutoWidth: false, 
+//     columns: [
+//         { title: "Dispute Date", sWidth: '20%' },
+//         { title: "Agent Profile", sWidth: '30%' },
+//         { title: "Decided By", sWidth: '30%'},
+//         { title: "Decision Favors", sWidth: '20%' }
+//     ]
+// });
 
-    jQuery('#profileDisputeOpendataTable').on( 'click', 'tr', function (e) {
+function initDisputeDecision() {
+    $('#profileDisputeCloseddataTable').DataTable( {
+        "processing": true,
+        "serverSide": true,
+        "bPaginate": true,
+        "bLengthChange": false,
+        "bFilter": false,
+        "bSort":false,
+        "bAutoWidth": false, 
+        "ajax": function(data, callback, settings) {
+            $.get(API_URL+'reclaim-dispute/decided/', {
+                page: offsetToPageno(data.start),
+            }, function(res) {
+                callback({
+                    recordsTotal: res.total,
+                    recordsFiltered: res.total,
+                    data: res.data
+                });
+            });
+        },
+        "columns": [
+            { 
+                data: "created_at", title: "Dispute Date", sWidth: '25%',
+                render: function(data, type, row, meta){
+                    return niceDate(data);
+                }
+            },
+            {   
+                data: null, title: "Agent Profile", sWidth: '25%' ,
+                render: function(data, type, row, meta){
+                    if (row.agent_profile_connector !== null) {
+                        var url = WEBSITE_URL+'page-three.html?agent_id='+row.connector.id;
+                        return "<a class='agent-profile-link' href='"+url+"' target='_blank'>"+row.connector.agent_name+"</a>";
+                    } else {
+                        return 'Not Found';
+                    }
+                    
+                }
+            },
+            {   
+                defaultContent: 'Anna', title: "Decided By", sWidth: '25%'
+            },
+            {   
+                data: "status", title: "Decision Favors", sWidth: '25%',
+                render: function(data, type, row, meta){
+                    return decisionInfavour(data);
+                }
+            }
+        ],
+        "createdRow": function (row, data, dataIndex) {
+            $(row).attr('data-id', data.id);
+        }
+    });
+}
+
+function disputeDetail() {
+    settings = get_settings('reclaim/'+reclaimId, 'GET');
+    settings['headers'] = {};
+    $.ajax(settings).done(function (response) {
+        var response = JSON.parse(response);
+        
+        if (response.agent_profile_connector !== null) {
+            $('.agent-name').text(response.connector.agent_name);
+        }
+
+        $('.dis-username').text(response.full_name);
+        $('.dis-email').text(response.email);
+        $('.dis-phone').text(response.phone);
+        $('.dis-date').text(niceDate(response.created_at));
+        
+        $('.dis-photoid').attr('src', response.id_picture);
+        $('.fancy-dis-photoid').attr('href', response.id_picture);
+
+        $('.dis-licenseid').attr('src', response.real_estate_license);
+        $('.fancy-dis-licenseid').attr('href', response.real_estate_license);
+
+        $('.closedReason').text(response.reason);
+        $('.decision-infavour').text(decisionInfavour(response.status));
+
+        if (response.status == 'pending') {
+            disputePendingShow();
+        } else {
+            disputePendingShow(false);
+        }
+        
+    }).fail(function(err) {
+        console.log(err.responseText);
+    });
+}
+
+function disputeUpdate(data) {
+    settings = get_settings('reclaim/'+reclaimId+'/', 'PUT', JSON.stringify(data));
+    settings['headers'] = null;
+    $.ajax(settings).done(function (response) {
+        disputePendingShow(false);
+    });
+}
+
+$(document).ready(function(){
+    initDisputePending();
+    initDisputeDecision();
+
+    $('#profileDisputeOpendataTable').on( 'click', 'tr', function (e) {
         if ($(e.target).attr('class') == 'agent-profile-link') {
             var url = $(e.target).attr('href');
             window.open(url);
             return false;
         }
 
-        jQuery('.profileDisputeInfo').show();
-        jQuery("#profileDisputeOpendataTable").parent().hide(); 
-        jQuery("#profileDisputeOpendataTable_wrapper").hide();
+        $('.profileDisputeInfo').show();
+        $("#profileDisputeOpendataTable").parent().hide(); 
+        $("#profileDisputeOpendataTable_wrapper").hide();
 
-        var reclaimId = $(this).data('id');
-
-        settings = get_settings('reclaim-dispute/'+reclaimId, 'GET');
-        settings['headers'] = {};
-        $.ajax(settings).done(function (response) {
-            var response = JSON.parse(response);
-            
-            if (response.agent_profile_connector !== null) {
-                $('.agent-name').text(response.connector.agent_name);
-            }
-
-            $('.dis-username').text(response.full_name);
-            $('.dis-email').text(response.email);
-            $('.dis-phone').text('111-222-3333');
-            $('.dis-date').text(niceDate(response.created_at));
-            
-            $('.dis-photoid').attr('src', response.id_picture);
-            $('.fancy-dis-photoid').attr('href', response.id_picture);
-
-            $('.dis-licenseid').attr('src', response.real_estate_license);
-            $('.fancy-dis-licenseid').attr('href', response.real_estate_license);
-            
-        }).fail(function(err) {
-            console.log(err.responseText);
-        });
+        reclaimId = $(this).data('id');
+        disputeDetail();
     });
 
-    // $(document).on('click', '.xxx', function (event) {
-    //     event.preventDefault();
-    //     event.stopPropagation();
-    // });
+    $('#profileDisputeCloseddataTable').on( 'click', 'tr', function () {
+        $('.closedProfileDisputeInfo').show();
+        $("#profileDisputeCloseddataTable").parent().hide(); 
+        $("#profileDisputeCloseddataTable_wrapper").hide();
+
+        reclaimId = $(this).data('id');
+        disputeDetail();
+    });
+
+    $('.completegetReasoncurOwn').on('click',function(){
+        $('.closedReason').html($('.getReasoncurOwn').val());
+        $('.decision-infavour').text('Current Owner');
+
+        var data = {
+            'reason': $('.getReasoncurOwn').val(),
+            'status': 'accept',
+        }
+        disputeUpdate(data);
+    });
+
+    $('.completgetReasondisputee').on('click',function(){
+        $('.closedReason').html($('.getReasondisputee').val());
+        $('.decision-infavour').text('Disputee');
+
+        var data = {
+            'reason': $('.getReasondisputee').val(),
+            'status': 'decline',
+        }
+        disputeUpdate(data);
+    });
+
+    $('#profileDisputeOpen-tab-classic').on('click', function(){
+        backOpenProfile();
+    });
+
+    $('#profileDisputeClosed-tab-classic').on('click', function(){
+        backOpenProfile();
+    });
+
+    $('.backIcon').on( 'click', function () {
+        backOpenProfile();
+    });
+
+    $('.closedProfileDisputeInfo-backIcon').on( 'click', function () {
+        backCloseProfile();
+    });
 });
