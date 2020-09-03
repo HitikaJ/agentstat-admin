78b816a2f44965e5038e4db079b5386a4a270927
Hassan Ejaz <hassanejaz22@gmail.com>
2020-09-04 01:58:38 +0500

    api url

--- codes/www/media/js/common.js
+++ codes/www/media/js/common.js
@@ -1,5 +1,5 @@
-// API_URL = 'https://app.agentstat.com/api/';
-API_URL = 'http://localhost:8000/api/';
+API_URL = 'https://app.agentstat.com/api/';
+// API_URL = 'http://localhost:8000/api/';
 WEBSITE_URL = 'https://agentstat.com/';
 
 function get_settings(url, method, data=null) {
